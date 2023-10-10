270370dc4d683dffb8d828058df92bd7077eb405
Shreya <101707492+ShreyaC11@users.noreply.github.com>
2023-10-10 13:17:11 +0530

    Changes 2

--- frontend/dsa_module/src/components/FrontPage/FrontPage.js
+++ frontend/dsa_module/src/components/FrontPage/FrontPage.js
@@ -3,22 +3,24 @@ import "./frontPage.css";
 
 function FrontPage() {
     return (
-        <div className="Main-container">
-            <div className='left-side'>
-                <div className="heading">
-                    <h1>Power Your Dreams <br/>with DSA Mastery!</h1>
-                    <p> “Where Algorithms Shapes Futures -<br></br>
-                        Your Journey to Success Begins Now” </p>
+      
+            <div className="Main-container">
+                <div className='left-side'>
+                    <div className="heading">
+                        <h1>Power Your Dreams <br />with DSA Mastery!</h1>
+                        <p> “Where Algorithms Shapes Futures -<br></br>
+                            Your Journey to Success Begins Now” </p>
+                    </div>
+                    <button className='button-1'> Hover Me</button>
                 </div>
-                <button className='button-1'> Hover Me</button>
-            </div>
-            <div className='right-side'>
-                <div className="image">
-                    <img src={process.env.PUBLIC_URL + "/images/output-onlinegiftools.gif"} alt="img" />
+                <div className='right-side'>
+                    <div className="image">
+                        <img src={process.env.PUBLIC_URL + "/images/output-onlinegiftools.gif"} alt="img" />
+                    </div>
                 </div>
+                
             </div>
-        </div>
-
+   
     );
 }
 export default FrontPage
--- frontend/dsa_module/src/components/Queries/Queries.js
+++ frontend/dsa_module/src/components/Queries/Queries.js
@@ -4,7 +4,44 @@ import "./Queries.css";
 function Queries() {
     return (
         <div className='Container'>
+            <div className='body'>
+                <div className='card'>
+                    <div className='Features-inner-card'>
+                        <div className='left'>
+                            <img className='icon' src={process.env.PUBLIC_URL + "/images/web-analytic1.png"} />
+                        </div>
+                        <div className='right'>
+                            <div className='inner-card-heading'>"Automated Heading"</div>
+                        </div>
+                        <img className='icon-outer' src={process.env.PUBLIC_URL + "/images/circle.png"}></img>
 
+                    </div>
+                </div>
+                <div className='card'>
+                    <div className='Features-inner-card'>
+                        <div className='left'>
+                            <img className='icon' src={process.env.PUBLIC_URL + "/images/web-analytic1.png"} />
+                        </div>
+                        <div className='right'>
+                            <div className='inner-card-heading'>"Automated Heading"</div>
+                        </div>
+                        <img className='icon-outer' src={process.env.PUBLIC_URL + "/images/circle.png"}></img>
+
+                    </div>
+                </div>
+                <div className='card'>
+                    <div className='Features-inner-card'>
+                        <div className='left'>
+                            <img className='icon' src={process.env.PUBLIC_URL + "/images/web-analytic1.png"} />
+                        </div>
+                        <div className='right'>
+                            <div className='inner-card-heading'>"Automated Heading"</div>
+                        </div>
+                        <img className='icon-outer' src={process.env.PUBLIC_URL + "/images/circle.png"}></img>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
