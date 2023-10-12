5e1a00206201c6b079037319f7518bc3fa80a73e
rautshreya <126368318+ShreyaR2409@users.noreply.github.com>
2023-10-12 20:06:40 +0530

    commit 2

--- frontend/dsa_module/src/components/CheatSheet/Sheet.js
+++ frontend/dsa_module/src/components/CheatSheet/Sheet.js
@@ -3,172 +3,53 @@ import "./sheet.css";
 export default function CheatSheet() {
 
     return (
-        <div className="Container">
-            {/* <h1 className="heading-1">Array Sorting Algorithms</h1> */}
-            <div className="body">
-                <div className="cards">
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Quick Sort</h1>
-                        </div>
-                        <div className="card-content" >
-                            <h3>Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n log(n))</p>
-                            <br></br>
-                            <p>Worst Case: O(n^2)</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Merge Sort</h1>
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n log(n))</p>
-                            <br></br>
-                            <p>Worst Case: O(n log(n))</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Heap Sort</h1>
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n log(n))</p>
-                            <br></br>
-                            <p>Worst Case: O(n log(n))</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Bubble Sort</h1>
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n)</p>
-                            <br></br>
-                            <p>Worst Case: O(n^2)</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Insertion Sort</h1>
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n)</p>
-                            <br></br>
-                            <p>Worst Case: O(n^2)</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Selection Sort</h1>
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n^2)</p>
-                            <br></br>
-                            <p>Worst Case: O(n^2)</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Radix Sort</h1>
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(nk)</p>
-                            <br></br>
-                            <p>Worst Case: O(nk)</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Heap Sort</h1>
-
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n log(n))</p>
-                            <br></br>
-                            <p>Worst Case: O(n log(n))</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Bucket Sort</h1>
-
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n +k)</p>
-                            <br></br>
-                            <p>Worst Case: O(n^2)</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Counting Sort</h1>
-
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n +k)</p>
-                            <br></br>
-                            <p>Worst Case: O(n +k)</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "25px 5px 0px 0px", }}>Tree<br></br> Sort</h1>
-
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n log(n))</p>
-                            <br></br>
-                            <p>Worst Case: O(n^2)</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="img-holder">
-                            <h1 style={{ textAlign: "center", fontFamily: "sans-serif", padding: "15px 5px 0px 0px", }}>Shell<br></br> Sort</h1>
-
-                        </div>
-                        <div className="card-content" >
-                            <h3 >Time Complexity</h3>
-                            <br></br>
-                            <p>Best Case: Ω(n log(n))</p>
-                            <br></br>
-                            <p>Worst Case: O(n^2)</p>
-                            {/* <a href="#">Read More</a> */}
-                        </div>
-                    </div>
-                </div>
+        <div>
+            <h1 className='cheetsheet-heading'>Sorting</h1>
+            <div className='programming-questions'>
+            <table class="tablecontent">
+          <thead>
+            <tr>
+              <th>ALGORITHMN</th>
+              <th colspan="3">TIME COMPLEXITY</th>
+              <th>SPACE COMPLEXITY</th>
+            </tr>
+            <tr>
+              <th></th>
+              <th>BEST</th>
+              <th>AVERAGE</th>
+              <th>WORST</th>
+              <th>WORST</th>
+            </tr>
+          </thead>
+          <tbody id="tbody">
+            <td>Quicksort</td>
+            <td>&Omega;(n log(n))</td>
+            <td>&Theta;(n log(n))</td>
+            <td>&Omicron;(n log(n))</td>
+            <td>&Omicron;(n log(n))</td>
+          </tbody>
+          <tbody id="tbody">
+            <td>Mergesort</td>
+            <td>&Omega;(n log(n))</td>
+            <td>&Theta;(n log(n))</td>
+            <td>&Omicron;(n log(n))</td>
+            <td>&Omicron;(n log(n))</td>
+          </tbody>
+          <tbody id="tbody">
+            <td>Heapsort</td>
+            <td>&Omega;(n log(n))</td>
+            <td>&Theta;(n log(n))</td>
+            <td>&Omicron;(n log(n))</td>
+            <td>&Omicron;(n log(n))</td>
+          </tbody>
+          <tbody id="tbody">
+            <td>Bubblesort(n)</td>
+            <td>&Omega;(n)</td>
+            <td>&Theta;(n*2)</td>
+            <td>&Omicron;(n log(n))</td>
+            <td>&Omicron;(n log(n))</td>
+          </tbody>
+        </table>
             </div>
         </div>
     );
--- frontend/dsa_module/src/components/Programming/Programming.js
+++ frontend/dsa_module/src/components/Programming/Programming.js
@@ -3,9 +3,67 @@ import "./Programming.css";
 
 function Programming() {
     return (
-        <div className='Container'>
+        <div>
+            <div className='filter'>
+                <div className="dropdown">
+                    <button className="dropbtn">Topic</button>
+                    <div className="dropdown-content">
+                        <a href="#">Array</a>
+                        <a href="#">String</a>
+                        <a href="#">Linked List</a>
+                    </div>
+                </div>
 
+                <div className="dropdown">
+                    <button className="dropbtn">Difficulty</button>
+                    <div className="dropdown-content">
+                        <a href="#">Easy</a>
+                        <a href="#">Medium</a>
+                        <a href="#">Hard</a>
+                    </div>
+                </div>
+                <div className="search-button">Search</div>
+                <input type="text" placeholder="Search Anything" className="search-input" />
+            </div>
+
+            <div className='programming-questions'>
+            <table class="tablecontent">
+          <thead>
+            <tr>
+              <th>SR NO.</th>
+              <th>TITLE</th>
+              <th>DIFFICULTY</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody id="tbody">
+            <td>1</td>
+            <td>Reverse Integer</td>
+            <td>Easy</td>
+            <td><button className='solve-button'>Solve</button></td>
+          </tbody>
+          <tbody id="tbody">
+            <td>2</td>
+            <td>Merge two sorted list</td>
+            <td>Easy</td>
+            <td><button className='solve-button'>Solve</button></td>
+          </tbody>
+          <tbody id="tbody">
+            <td>3</td>
+            <td>Sudoku Solver</td>
+            <td>Hard</td>
+            <td><button className='solve-button'>Solve</button></td>
+          </tbody>
+          <tbody id="tbody">
+            <td>4</td>
+            <td>Valid Parentheses</td>
+            <td>Medium</td>
+            <td><button className='solve-button'>Solve</button></td>
+          </tbody>
+        </table>
+            </div>
         </div>
     );
 }
-export default Programming
+
+export default Programming;
