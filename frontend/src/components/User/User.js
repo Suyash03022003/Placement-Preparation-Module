ca0d1fdea475f6772c3e41e84365a5b7f6a532eb
rautshreya <126368318+ShreyaR2409@users.noreply.github.com>
2023-11-20 13:04:17 +0530

    User updated

--- frontend/src/components/User/User.js
+++ frontend/src/components/User/User.js
@@ -1,47 +1,46 @@
-import React from 'react'
-import {
-    Route,
-    Routes
-} from "react-router-dom"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import FrontPage from '../FrontPage/FrontPage.js';
-import Programming from '../Programming/Programming.js';
-import DsaSyllabus from '../DsaSyllabus/DsaSyllabus.js';
-import CheatSheet from '../CheatSheet/Sheet.js';
-import ShowDetailsQues from '../Programming/ShowDetailsQues.js';
-import Register from '../Register/Register.js';
-import Array from '../Array.js';
-import String from '../String.js';
-import Discuss from '../Discuss/Discuss.js'
-import Profile from '../UserProfile/Profile.js';
-import LearnDSA from '../LearnDSA/LearnDSA.js';
-import InterviewQues from '../InterviewQues/InterviewQues.js';
-import Quizzes from '../Quizzes/Quizzes.js';
-import Navbar from '../NavBar/Navbar.js';
-import Footer from '../Footer/Footer.js';
+import FrontPage from "../FrontPage/FrontPage.js";
+import Programming from "../Programming/Programming.js";
+import DsaSyllabus from "../DsaSyllabus/DsaSyllabus.js";
+import CheatSheet from "../CheatSheet/Sheet.js";
+import ShowDetailsQues from "../Programming/ShowDetailsQues.js";
+import Register from "../Register/Register.js";
+import Array from "../Array.js";
+import String from "../String.js";
+import Discuss from "../Discuss/Discuss.js";
+import Profile from "../UserProfile/Profile.js";
+import LearnDSA from "../LearnDSA/LearnDSA.js";
+import InterviewQues from "../InterviewQues/InterviewQues.js";
+import Quizzes from "../Quizzes/Quizzes.js";
+import Navbar from "../NavBar/Navbar.js";
+import Footer from "../Footer/Footer.js";
 
 const User = () => {
-    return (
-        <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<FrontPage />} />
-                <Route path="/programming" element={<Programming />} />
-                <Route path="/dsasyllabus" element={<DsaSyllabus />} />
-                <Route path="/array" element={<Array />} />
-                <Route path="/string" element={<String />} />
-                <Route path="/cheatsheet" element={<CheatSheet />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/discuss/*" element={<Discuss />} />
-                <Route path="/learnDSA" element={<LearnDSA />} />
-                <Route path="/quizzes" element={<Quizzes />} />
-                <Route path="/interviewques" element={<InterviewQues />} />
-                <Route path="/register" element={<Register />} />
-                <Route path='/details/:id' element={<ShowDetailsQues />} />
-            </Routes>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/programming" element={<Programming />} />
+        <Route path="/dsasyllabus" element={<DsaSyllabus />} />
+        <Route path="/array" element={<Array />} />
+        <Route path="/string" element={<String />} />
+        <Route path="/cheatsheet" element={<CheatSheet />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/discuss/*" element={<Discuss />} />
+        <Route path="/learnDSA" element={<LearnDSA />} />
+        <Route path="/quizzes" element={<Quizzes />} />
+        <Route path="/interviewques" element={<InterviewQues />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<ShowDetailsQues />} />
+        <Route path="/quizquestion" element={<QuizQuestion />} />
+        <Route path="/quizzes" element={<Quizzes />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
 
-export default User
+export default User;
